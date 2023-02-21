65933f67e42e0761603ed9c7f5b697ccd6e8b028
Júlia Silva <julia_vix@yahoo.com.br>
2023-02-21 20:10:11 -0300

    teste

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 router.get('/', function (req, res, next) {
     res.status(200).send({
-        title: "Teste PC",
+        title: "PC Júlia",
         version: "0.0.1"
     });
 });
