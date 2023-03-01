2951909f0c7d26790cf6794e9331ea69ec908dcf
Sala 18A <sala18a@ceines.com.br>
2023-02-28 21:33:58 -0300

    28/02/23

--- src/routes/criptoRouter.js
+++ src/routes/criptoRouter.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/criptoController')
 
 //router.post('/adiciona-moeda', controller.adicionaMoeda)
-router.get('/api/cripto/obter-moeda', controller.obterMoedas)
-router.get('/api/cripto/obter-moeda/peloid?id=', controller.obterMoedaPeloId)
+router.get('/obter-moedas', controller.obterMoedas)
+router.get('/obter-moeda/peloId', controller.obterMoedaPeloId)
 //router.delete('/deleta-moeda', controller.deletaMoeda)
 //router.put('/altera-moeda', controller.alteraMoeda)
 
